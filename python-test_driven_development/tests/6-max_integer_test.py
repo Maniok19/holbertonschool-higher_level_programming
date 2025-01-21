54dc77101e70cbc4db9d0405e7bdc3266b16361e
Maniok19 <mano19029@hotmail.fr>
2025-01-21 10:26:02 +0100

    Add additional test cases for max_integer function

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,4 +8,13 @@ class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-        
+        self.assertEqual(max_integer([1, 3, 2, 4]), 4)
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([1, 1, 1, 2]), 2)
+        self.assertEqual(max_integer([2, 1, 1, 1]), 2)
+        self.assertEqual(max_integer([1, 2, 1, 1]), 2)
+        self.assertEqual(max_integer([1, 1, 2, 1]), 2)
+        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
