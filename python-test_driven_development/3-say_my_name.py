51b499ca3027bbdf74546f1b47e4bfb0311c21f4
Maniok19 <mano19029@hotmail.fr>
2025-01-21 14:23:24 +0100

    Refactor say_my_name function for improved output formatting and enhance test cases for better coverage of edge cases

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -11,13 +11,8 @@ def say_my_name(first_name, last_name=""):
     None"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
+
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    if first_name == "" and last_name == "":
-        print("My name is")
-    elif first_name == "":
-        print("My name is {:s}".format(last_name))
-    elif last_name == "":
-        print("My name is {:s}".format(first_name))
-    else:
-        print("My name is {:s} {:s}".format(first_name, last_name))
+
+    print("My name is {} {}".format(first_name, last_name))
