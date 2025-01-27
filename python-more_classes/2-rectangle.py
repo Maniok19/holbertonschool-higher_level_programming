95abbc09a7a4a345030b2d752be613c41918ba53
Maniok19 <mano19029@hotmail.fr>
2025-01-27 11:39:16 +0100

    Refactor Rectangle class to use private attributes for width and height in area and perimeter methods

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -39,8 +39,8 @@ class Rectangle:
 
     def area(self):
         """Returns the area of the rectangle."""
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """Returns the perimeter of the rectangle."""
-        return 2 * (self.width + self.height)
+        return 2 * (self.__width + self.__height)
--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -43,17 +43,17 @@ class Rectangle:
 
     def area(self):
         """Returns the area of the rectangle."""
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """Returns the perimeter of the rectangle."""
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
         """Returns the rectangle with the character #."""
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ""
         return "{}".format('\n'.join([str(self.print_symbol) * self.width for
                                      i in range(self.height)]))
