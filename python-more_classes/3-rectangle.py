d6920b72bcee4a00ed14c1ef1165a5ecec032c20
Maniok19 <mano19029@hotmail.fr>
2025-01-27 13:11:16 +0100

    Fix Rectangle class to use isinstance for type checks and update string representation for zero dimensions

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Rectangle class with a private instance attribute width and height"""
     def __init__(self, width=0, height=0):
         """Initializes the data."""
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -17,7 +17,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Sets the width of the rectangle."""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -31,7 +31,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the height of the rectangle."""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -49,9 +49,10 @@ class Rectangle:
 
     def __str__(self):
         """Returns the rectangle with the character #."""
-        return "{}".format('\n'.join(['#' * self.width for
-                                     i in range(self.height)]))
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         """Returns the string representation of the rectangle."""
-        return "Rectangle({}, {})".format(self.width, self.height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
