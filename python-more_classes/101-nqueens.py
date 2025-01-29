19f2d951d31848a622760966c54170d6755fd90c
Maniok19 <mano19029@hotmail.fr>
2025-01-29 11:02:23 +0100

    Fixed print multiple solution in nqueen

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,76 +1,60 @@
-#!/usr/bin/python3
-"""N queens puzzle solver"""
-
+#!/usr/bin/env python3
 import sys
 
-
-def printSolution(board, size):
-    """Print the solution in the required format"""
-    solution = []
-    for i in range(size):
-        for j in range(size):
-            if board[i][j] == 1:
-                solution.append([i, j])
-    print(solution)
-
-
-def isSafe(board, row, col, size):
-    """Check if a queen can be placed on board[row][col]"""
+def is_safe(board, row, col, n):
     # Check this row on left side
     for i in range(col):
         if board[row][i] == 1:
             return False
-
+    
     # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
+    
     # Check lower diagonal on left side
-    for i, j in zip(range(row, size, 1), range(col, -1, -1)):
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
+    
     return True
 
-
-def solveNQUtil(board, col, size):
-    """Utility function to solve N Queen Problem"""
-    if col >= size:
+def solve_nqueens(board, col, n):
+    if col >= n:
+        print_solution(board)
         return True
-
-    for i in range(size):
-        if isSafe(board, i, col, size):
+    
+    for i in range(n):
+        if is_safe(board, i, col, n):
             board[i][col] = 1
-            if solveNQUtil(board, col + 1, size):
-                return True
+            solve_nqueens(board, col + 1, n)
             board[i][col] = 0
 
-    return False
+def print_solution(board):
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
 
-
-def solveNQ(size):
-    """Initialize the board and solve the N Queen Problem"""
-    board = [[0 for x in range(size)] for y in range(size)]
-
-    if solveNQUtil(board, 0, size) is False:
-        print("Solution does not exist")
-        return False
-
-    printSolution(board, size)
-    return True
-
+def nqueens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solve_nqueens(board, 0, n)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+    
     try:
-        size = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if size < 4:
+    
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    solveNQ(size)
+    
+    nqueens(n)
