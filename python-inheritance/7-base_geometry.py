89f034fcbc3e3c5329ca64ace4f6d55e5aadaed8
Maniok19 <mano19029@hotmail.fr>
2025-02-03 13:19:47 +0100

    Refactor docstrings in BaseGeometry class for clarity and specificity

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
-""" Module for BaseGeometry class """
+"""Module that defines a base geometry class"""
 
 
 class BaseGeometry:
-    """A class for basic geometric operations."""
+    """Defines methods for geometric shapes."""
 
     def area(self):
-        """Raises an Exception indicating that the method
-        is not implemented."""
+        """Calculate the area of a geometric shape.
+
+        Raises:
+            Exception: if the area method is not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validate an integer value.
+
+        Args:
+            name (str): the name of the value.
+            value (int): the value to validate.
+
+        Raises:
+            TypeError: if the value is not an integer.
+            ValueError: if the value is less than or equal to 0.
         """
-        Validates that the value is an integer and greater than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
