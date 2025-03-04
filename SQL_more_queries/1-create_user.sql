fb0c9e0ad779c15ea9728ef40d99194002811599
Maniok19 <mano19029@hotmail.fr>
2025-03-04 09:52:39 +0100

    Enhance SQL scripts by adding user existence check, refining join types, and improving order clauses for better query performance

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Wabwabalooda do wapwapwapwa johnny tu nous manques
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
