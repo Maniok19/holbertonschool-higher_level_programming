bc82d31d89772ecc77070221d54d1f7c34001718
Maniok19 <mano19029@hotmail.fr>
2025-03-05 13:58:40 +0100

    Refactor engine creation for improved readability in 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,11 +9,13 @@ from model_state import State
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format\
-                           (sys.argv[1],\
-                            sys.argv[2],\
-                            sys.argv[3]),\
-                            pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        )
+    )
     session = Session(engine)
     results = session.query(State).order_by(State.id).all()
     for result in results:
