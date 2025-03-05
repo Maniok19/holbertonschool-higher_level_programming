5e9fe645cc0b36d038711c27ab7d4d5fd4969ab8
Maniok19 <mano19029@hotmail.fr>
2025-03-05 13:22:15 +0100

    Add metadata support to the declarative base in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,14 +2,17 @@
 """
 wabababababababjdbjbj
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-
-Base = declarative_base()
+meta = MetaData()
+Base = declarative_base(metadata=meta)
 
 
 class State(Base):
+    """
+    State class
+    """
     __tablename__ = 'states'
     id = Column(Integer,
                 autoincrement=True,
