2fb29195426e1e089eba058bc924d4c9fa36d1d9
Maniok19 <mano19029@hotmail.fr>
2025-03-05 09:54:08 +0100

    Refactor query in select_states.py to simplify SQL statement and improve readability

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""This module makes a query to the database hbtn_0e_0_usa"""
-
-
-import MySQLdb
+"""
+This module makes a query to the database hbtn_0e_0_usa
+"""
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
 
     db = MySQLdb.connect(
-        host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
