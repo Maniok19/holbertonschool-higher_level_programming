6f6f433015d1252df6e28482e1b46bf25ebea89c
Maniok19 <mano19029@hotmail.fr>
2025-03-22 15:11:43 +0100

    Store generated filenames in a list and simplify attendee field handling in invitation generation

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -28,17 +28,18 @@ def generate_invitations(template, attendees):
         print("No data provided, no output files generated.")
         return []
 
+    g_file = []
     required_fields = ["name", "event_title", "event_date", "event_location"]
     for index, attendee in enumerate(attendees, start=1):
         replaced = template
         for field in required_fields:
-            value = attendee.get(field)
-            if value is None:
-                value = f'{field}: N/A'
-            str_value = str(value) if not isinstance(value, str) else value
-            replaced = replaced.replace(f'{{{field}}}', str_value)
+            value = attendee.get(field, "N/A")
+            replaced = replaced.replace(f'{{{field}}}', value)
 
         filename = f'output_{index}.txt'
 
         with open(filename, 'w') as file:
             file.write(replaced)
+        g_file.append(filename)
+
+    return g_file
